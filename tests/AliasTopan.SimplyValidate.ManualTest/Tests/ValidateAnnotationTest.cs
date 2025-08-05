aeaaa02eba647e078bfa0396112c16b2abb4b5ca
Taufan Augusta <taufan.augusta@protonmail.com>
2025-08-05 21:05:16 +0700

    proper manual test using `EitherPattern`

--- tests/AliasTopan.SimplyValidate.ManualTest/Tests/ValidateAnnotationTest.cs
+++ tests/AliasTopan.SimplyValidate.ManualTest/Tests/ValidateAnnotationTest.cs
@@ -1,3 +1,5 @@
+using System.Text;
+using AliasTopan.EitherPattern;
 using AliasTopan.SimplyValidate.Models;
 
 namespace AliasTopan.SimplyValidate.Tests;
@@ -9,10 +11,13 @@ public static class ValidateAnnotationTest
         Console.WriteLine("# _ValidateAnnotationTest_");
 
         BadRequestExample();
+        GoodRequestExample();
     }
 
     private static void BadRequestExample()
     {
+        Console.WriteLine("Bad Request:");
+
         var badRequest = new NewAccountRequest
         {
             Username = "e",
@@ -20,20 +25,76 @@ public static class ValidateAnnotationTest
             Password = "weak"
         };
 
-        var isValid = badRequest.ValidateAnnotation(out var complianceErrors);
+        var result = CreateAccount(badRequest);
 
-        if (isValid)
+        result.Match(
+            onSuccess: _ => Console.WriteLine("This should not happen."),
+            onError: errors => Console.WriteLine($"{errors.Message}")
+        );
+
+        Console.Write("\n");
+    }
+
+    private static void GoodRequestExample()
+    {
+        Console.WriteLine("Good Request:");
+
+        var goodRequest = new NewAccountRequest
         {
-            Console.WriteLine("Status Code 200");
+            Username = "aliastopan",
+            Email = "topan@example.com",
+            Password = "secure_password"
+        };
 
-            return;
+        var result = CreateAccount(goodRequest);
+
+        result.Match(
+            onSuccess: _ => Console.WriteLine("Request accepted."),
+            onError: _ => Console.WriteLine("This should not happen.")
+        );
+
+        Console.Write("\n");
+    }
+
+    public static Either<CreateAccountError, Success> CreateAccount(NewAccountRequest request)
+    {
+        if (!request.ValidateAnnotation(out var annotationErrors))
+        {
+            CreateAccountError error = new CreateAccountError(annotationErrors);
+
+            return Either<CreateAccountError, Success>.Error(error);
         }
 
-        Console.WriteLine("Status Code 422");
+        return Either<CreateAccountError, Success>.Success(Success.Default);
+    }
+}
 
-        foreach (var error in complianceErrors)
+public record CreateAccountError
+{
+    public IReadOnlyCollection<AnnotationError> AnnotationErrors { get; init; }
+    public string Message => FormatErrorMessage();
+
+    public CreateAccountError(IReadOnlyCollection<AnnotationError> annotationErrors)
+    {
+        AnnotationErrors = annotationErrors;
+    }
+
+    private string FormatErrorMessage()
+    {
+        if (AnnotationErrors is null || AnnotationErrors.Count is 0)
         {
-            Console.WriteLine($" - Member: {error.MemberName}, Error: {error.ErrorMessage}");
+            return string.Empty;
         }
+
+        StringBuilder builder = new StringBuilder();
+        builder.AppendLine("Account creation failed due to the following annotation errors:");
+
+        int i = 1;
+        foreach (var error in AnnotationErrors)
+        {
+            builder.AppendLine($" {i++}. Member: '{error.MemberName}', Reason: {error.ErrorMessage}");
+        }
+
+        return builder.ToString();
     }
 }

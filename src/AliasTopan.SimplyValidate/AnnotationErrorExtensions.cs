868999ce777e515fd247f84bf948b1333d180efe
Taufan Augusta <taufan.augusta@protonmail.com>
2025-08-30 23:42:08 +0700

    feat: add extension method for ValidationError collection

---
+++ src/AliasTopan.SimplyValidate/AnnotationErrorExtensions.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AliasTopan.SimplyValidate
+{
+    public static class AnnotationErrorExtensions
+    {
+        public static string GetMessage(this IReadOnlyCollection<ValidationError> errors)
+        {
+            if (errors.Count == 0)
+            {
+                return "An unspecified validation error occurred.";
+            }
+
+            return errors.First().ErrorMessage;
+        }
+
+        public static string GetMessageVerbose(this IReadOnlyCollection<ValidationError> errors)
+        {
+            if (errors.Count == 0)
+            {
+                return "No validation errors were found.";
+            }
+
+            StringBuilder builder = new StringBuilder();
+            builder.AppendLine($"Validation failed with {errors.Count} error(s):");
+
+            int i = 1;
+            foreach (var error in errors)
+            {
+                builder.AppendLine($"  {i++}. [{error.MemberName}]: {error.ErrorMessage}");
+            }
+
+            return builder.ToString().TrimEnd();
+        }
+    }
+}
